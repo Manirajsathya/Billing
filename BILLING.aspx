de5406adb229ea0fc66235cc11ed301774b2abe2
Manirajsathya <130271387+Manirajsathya@users.noreply.github.com>
2023-11-29 13:49:33 +0530

    Billing

---
+++ BILLING.aspx
@@ -0,0 +1,110 @@
+﻿ <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BILLING.aspx.cs" Inherits="WebApplication_BILLING.BILLING" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Vegetables Billing</title>
+    <link rel="icon" href="carrot.png"  />
+    <link rel="stylesheet" href="StyleSheet1.css" />
+</head>
+<body style="background-color:gainsboro">
+    <form id="form1" runat="server">
+        
+        <div class="header">
+            <h1 style="padding-bottom:10px">  Fresh Vegetables </h1>
+            <img src="vegetables-all.png" width="500 px" height="200px" />
+        </div>
+        <center> 
+        <div class="input"> 
+
+            <table>
+                <tr>     <td><asp:Label runat="server" ID="IdVegetableLabel" Text="VEGETABLE"></asp:Label> </td>               
+                       <td> <asp:DropDownList runat="server" ID="IdVegDrop"></asp:DropDownList></td>
+                    <td><asp:Label runat="server" ID="IdWeightLabel" Text="WEIGHT"></asp:Label> </td>
+                        
+                        <td><asp:TextBox runat="server" ID="IdWeightTextBox"></asp:TextBox></td>
+                   
+                    </tr>
+                     <tr>
+                         <td></td>
+                    <td><asp:Button runat="server" ID="IdAddButton" Text="ADD" OnClick="IdAddButton_Click" /> </td>                    
+                         <td><asp:Button runat="server" ID="Idclear" Text="Clear" OnClick="Idclear_Click" /> </td>
+            </table>
+            </div>
+            </center>           
+    
+            
+                 <div class="left"> 
+
+            <asp:GridView runat="server" ID="GridView2" AutoGenerateColumns="false" OnRowUpdating="GridView2_RowUpdating" OnRowEditing="GridView2_RowEditing" OnRowDeleting="GridView2_RowDeleting1" ShowFooter="true" > 
+                <Columns>
+                    <asp:TemplateField HeaderText="Id">
+                        <ItemTemplate> 
+                            <asp:TextBox runat="server" ID="IdTextBox" Text='<%# Bind("Id") %>'  Width="20px" ></asp:TextBox>  
+                        </ItemTemplate>
+                        
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Vegetable" >
+                        <EditItemTemplate>
+                            <asp:TextBox runat="server" ID="VegText" Text='<%# Bind("Vegetable") %>' Width="50px"  ></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="VegLabel" Text='<%# Bind ("Vegetable") %>' Width="50px" ></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Weight">
+                        <EditItemTemplate>
+                            <asp:TextBox runat="server" ID="WeightTextBox" Text='<%#Bind ("Weight") %>' Width="50px" ></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="WeightLabel" Text='<%# Bind("Weight")%>' Width="50px" ></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Price/Kg"  >
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="PriceKgLabel" Text='<%# Bind("[Price/Kg]")%>' Width="50px"  ></asp:Label>
+                        </ItemTemplate>
+                        
+                            
+                    </asp:TemplateField>
+                     
+                    <asp:TemplateField HeaderText="Total" >
+                        <EditItemTemplate>
+                            <asp:TextBox runat="server" ID="TotalTextBox" Text='<%#Bind ("Total") %>' ></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="TotalLabel" Text='<%# Bind("Total")%>'  ></asp:Label>
+                        </ItemTemplate>
+                        <FooterTemplate>
+                <asp:Label ID="TotalLabel" runat="server" Text="Total" ></asp:Label>
+                            
+            </FooterTemplate>
+                        
+
+                    </asp:TemplateField>
+                    
+                    <asp:TemplateField HeaderText="Action">
+
+                        <ItemTemplate>
+                            <asp:Button runat="server" ID="IdBUtton1" Text="Edit" CommandName="Edit"  />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:Button runat="server" ID="IdBUtton2" Text="Update" CommandName="Update"  />
+                            <asp:Button runat="server" ID="IdBUtton3" Text="Delete" CommandName="Delete"  />
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+
+
+                </Columns> 
+                </asp:GridView>                      
+                     </div>               
+                     <div class="right" > 
+              <asp:GridView runat="server" ID="GridView1" BackColor="WhiteSmoke" RowStyle-BackColor="Window"   ></asp:GridView>
+                </div>
+             
+            
+    </form>
+</body>
+</html>
---
+++ BILLING.aspx.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication_BILLING
+{
+
+    public partial class BILLING : System.Web.UI.Page
+    {
+        string connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+
+                    connection.Open();
+                    string selectquery = "select * from [practice].[dbo].[List]";
+                    SqlCommand cmd = new SqlCommand(selectquery, connection);
+                    SqlDataReader Reader = cmd.ExecuteReader();
+                    //IdVegDrop.DataSource = Reader;
+                    //IdVegDrop.DataBind();
+                    //IdVegDrop.DataTextField = "Vegetables";   here reader not required 
+                    //IdVegDrop.DataValueField = "Vegetables";
+                    //IdVegDrop.DataBind();
+                    connection.Close();
+
+                    SqlDataAdapter Adapter = new SqlDataAdapter(cmd);
+                    DataTable Dt = new DataTable();
+                    Adapter.Fill(Dt);
+                    IdVegDrop.DataSource = Dt;
+                    IdVegDrop.DataTextField = "Vegetables";
+                    //IdVegDrop.DataValueField = "Id";      //without this also working properly 
+                    IdVegDrop.DataBind();
+                    connection.Close();
+                }
+                BindList();
+
+
+            }
+        }
+        decimal TotalBill = 0;
+        protected void IdAddButton_Click(object sender, EventArgs e)
+        {
+            string Vegetable = IdVegDrop.SelectedValue;
+            //int Vegetable_Id = int.Parse(IdVegTextBox.Text);
+            decimal User_Weight = decimal.Parse(IdWeightTextBox.Text);
+            string formattedWeight = User_Weight.ToString("0.000");
+
+
+            DataTable Table = new DataTable();
+            Table.Columns.Add("Id", typeof(int));
+            Table.Columns.Add("Vegtable", typeof(string));
+            Table.Columns.Add("Weight", typeof(decimal));
+            Table.Columns.Add("Price/Kg", typeof(decimal));
+            Table.Columns.Add("Total", typeof(decimal));
+
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string selectquery = "select * from [practice].[dbo].[List] where Vegetables=@Vegetable";
+                SqlCommand cmd = new SqlCommand(selectquery, connection);
+                cmd.Parameters.AddWithValue("@Vegetable", Vegetable);
+                SqlDataReader Reader = cmd.ExecuteReader();
+
+
+                if (Reader.Read())
+                {
+                   
+                    int Id = (int)Reader["Id"];
+                    string vegetableName = Reader["Vegetables"].ToString();
+                    decimal PricePerKg = (decimal)Reader["Price/Kg"];
+                    decimal Total = PricePerKg * User_Weight;
+                    TotalBill += Total;
+
+
+                    Insert(Id, vegetableName, User_Weight, PricePerKg, Total); /*vegetableName*/
+
+                    BindList2();
+                }
+
+            }
+        
+    
+        }
+        private void Insert(int Id, string vegetableName, decimal User_Weight, decimal PricePerUnit, decimal Total)
+
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string InsertQuery = "insert into[practice].[dbo].[Bill](Id,Vegetable,Weight,[Price/Kg],Total)values(@Id,@vegetableName,@User_Weight,@pricePerUnit,@totalBill) ";
+                SqlCommand cmd = new SqlCommand(InsertQuery, connection);
+                //SqlDataReader Reader = cmd.ExecuteReader();
+                cmd.Parameters.AddWithValue("@Id", Id);
+                cmd.Parameters.AddWithValue("@vegetableName", vegetableName);
+                cmd.Parameters.AddWithValue("@User_Weight", User_Weight);
+                cmd.Parameters.AddWithValue("@pricePerUnit", PricePerUnit);
+                cmd.Parameters.AddWithValue("@totalBill", Total);
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+
+
+        }
+
+
+        protected void IdEndButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BindList()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string selectquery = "select * from [practice].[dbo].[List]";
+                SqlCommand cmd = new SqlCommand(selectquery, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+
+
+            }
+
+        }
+
+        private void BindList2()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string selectquery = "select * from [practice].[dbo].[Bill]";
+                SqlCommand cmd = new SqlCommand(selectquery, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                GridView2.DataSource = ds;
+                decimal totalSalary = 0;
+                foreach (DataRow dr in ds.Tables[0].Rows)   
+                {
+                    totalSalary += Convert.ToDecimal(dr["Total"]);
+                }
+                GridView2.Columns[5].FooterText = totalSalary.ToString();
+                GridView2.DataBind();
+
+
+
+            }
+
+        }
+
+
+
+        protected void GridView2_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            {
+                TextBox IdTextBox = (TextBox)GridView2.Rows[e.RowIndex].FindControl("IdTextBox");
+                TextBox VegText = (TextBox)GridView2.Rows[e.RowIndex].FindControl("VegText");
+                TextBox WeightTextBox = (TextBox)GridView2.Rows[e.RowIndex].FindControl("WeightTextBox");                
+                //TextBox TotalTextBox = (TextBox)GridView2.Rows[e.RowIndex].FindControl("TotalTextBox");
+                int Id = int.Parse(IdTextBox.Text);
+                string Vegetable = VegText.Text;
+                decimal User_Weight = decimal.Parse(WeightTextBox.Text);
+                //decimal Total = decimal.Parse(TotalTextBox.Text);
+                decimal UpdateTotal = 0;
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+                    string selectquery = "select * from [practice].[dbo].[List] where Vegetables=@Vegetable";
+                    SqlCommand cmd = new SqlCommand(selectquery, connection);
+                    cmd.Parameters.AddWithValue("@Vegetable", Vegetable);
+                    SqlDataReader Reader = cmd.ExecuteReader();
+                    if (Reader.Read())
+                    {
+                        //int Id = (int)Reader["Id"];
+                        string vegetableName = Reader["Vegetables"].ToString();
+                        decimal PricePerKg = (decimal)Reader["Price/Kg"];
+                         UpdateTotal = PricePerKg * User_Weight;
+                        //UpdateTotal += Total;
+                        
+                    }
+                }
+
+
+
+                        using (SqlConnection connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+                    string UpdateQuery = "update [practice].[dbo].[Bill] set Vegetable=@Vegetable, Weight=@Weight,Total=@totalBill where Id=@Id ";
+                    SqlCommand cmd = new SqlCommand(UpdateQuery, connection);
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    cmd.Parameters.AddWithValue("@Vegetable", Vegetable);
+                    cmd.Parameters.AddWithValue("@Weight", User_Weight);                   
+                    cmd.Parameters.AddWithValue("@totalBill", UpdateTotal);
+                    cmd.ExecuteNonQuery();
+                    BindList2();
+                }
+            }
+        }
+
+        protected void GridView2_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView2.EditIndex = e.NewEditIndex;
+            BindList2();
+        }
+
+        
+
+        protected void GridView2_RowDeleting1(object sender, GridViewDeleteEventArgs e)
+        {
+            TextBox Textid = (TextBox)GridView2.Rows[e.RowIndex].FindControl("IdTextBox");
+            int Id = int.Parse(Textid.Text);
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string DeleteQuery = " Delete from [practice].[dbo].[Bill] where Id=@Id";
+                SqlCommand cmddltquery = new SqlCommand(DeleteQuery, connection);
+                cmddltquery.Parameters.AddWithValue("@Id", Id);
+                cmddltquery.ExecuteNonQuery();
+            }
+        }
+
+        protected void Idclear_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
+                string TruncateQuery = "Truncate table [practice].[dbo].[Bill]";
+                SqlCommand cmddltquery = new SqlCommand(TruncateQuery, connection);                
+                cmddltquery.ExecuteNonQuery();
+            }
+
+
+
+        }
+    }
+    }
+
